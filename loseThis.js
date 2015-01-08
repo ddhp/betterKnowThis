ba7707ef7de571aeeb3b14611c6a8e1dc8237144
jessechen <jessechen@kkbox.com>
2015-01-08 10:46:35 +0800

    update explaination

--- loseThis.js
+++ loseThis.js
@@ -1,31 +1,35 @@
 var Q = require('q');
 
+// a person always sing and walk
+// first, a person is given a name
+// he wants to sing but it takes a bit of time to recognize it
+// after he starts singing, he also start walking
 var Person = function (name) {
   var name = name,
-      song = new Song(),
+      song = new Song(), // the song this person knows
 
       walk = function () {
         var self = this;
         console.log('yes, I am walking now, and `this` at walk: ', this);
         setTimeout(function () {
           console.log('`this` in timeout callback: ', this)
-        }.bind(self), 500)
+        }, 500)
       },
 
       sing = function () {
         console.log('`this` when sing: ', this);
         var self = this;
         song.getName().then(function (name) {
+          console.log('this in defer callback: ', this.constructor);
           console.log('I am singing ', name);
           console.log('can I walk here?');
           walk.call(self); // yes
           console.log('can I recognize my name here?')
-          if (self.name !== undefined && self.name !== null) {
-            console.log('yes! my name is ', self.name);
-          } else {
-            console.log('no.. I don\'t know my name. Is it', self.name);
-          }
-          console.log('maybe I can get it from a method.');
+          console.log('not with "this", this.name = ', this.name);
+          console.log('neither with "self", self.name = ', self.name);
+          console.log('because it\'s not expose outside of class');
+          console.log('actually if "name" isn\'t being overwritten, I can get it with *name*');
+          console.log('also I can get it from a method.');
           console.log('cool, my name is', self.getName());
         })
       },
